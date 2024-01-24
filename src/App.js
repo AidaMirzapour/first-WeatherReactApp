da37f327f2f98db047ee2c7e2bb90d5ce6586a0e
Itsaida <113546067+Itsaida@users.noreply.github.com>
2024-01-24 18:52:12 +0330

    add FooterLinks componant

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import Weather from "./Weather";
+import FooterLinks from "./FooterLinks";
 
 function App() {
   return (
     <div className="App">
       <h1>Weather app</h1>
       <Weather />
+      <FooterLinks className="FooterLinks"/>
     </div>
   );
 }
---
+++ src/FooterLinks.js
@@ -0,0 +1,27 @@
+import React from "react";
+export default function FooterLinks() {
+  return (
+    <div className="FooterLinks">
+      <a
+        href="https://github.com/AidaMirzapour/Weather-app-React"
+        target="_blank"
+        rel="noreferrer"
+      >
+        Open-source code
+      </a>
+      , by{" "}
+      <a
+        href="https://github.com/AidaMirzapour"
+        target="_blank"
+        rel="noreferrer"
+      >
+        Aida Mirzapour
+      </a>{" "}
+      from{" "}
+      <a href="https://www.shecodes.io" target="_blank" rel="noreferrer">
+        She Codes
+      </a>
+      .
+    </div>
+  );
+}
