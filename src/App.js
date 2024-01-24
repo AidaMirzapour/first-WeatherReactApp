bfdf5f7ccf96975802ed6c291f17bfbc89abd60c
Itsaida <113546067+Itsaida@users.noreply.github.com>
2024-01-24 18:29:59 +0330

    imported React files

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import Weather from "./Weather";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Weather app</h1>
+      <Weather />
+    </div>
+  );
+}
+
+export default App;
